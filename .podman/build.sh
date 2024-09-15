8dcd95027a56862481a6e2ca08c0b2d60f749644
Iain Crawford (dakodun) <github@chikin.net>
2024-09-15 13:08:25 +0000

    changed dev container setup
    removed alpine image (git/openssh) and instead added node image which includes git/openssh as well as vitest/istanbul

--- .podman/build.sh
+++ .podman/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -x
 
-# simple script for initialising a pod consisting of alpine (w/ git) and
-# nginx serving the project directory
+# simple script for initialising a pod consisting of alpine
+# (w/ npm/vitest & git/openssh) and nginx serving the project directory
+# to localhost:8080
 
 # to use, cd into the .podman folder and run the script, attach
-# to the alpine container with vscode via the dev containers extension
-# and then open folder /workspace/solastjs
+# to the node container (sol-node) with vscode via the dev containers
+# extension and then open folder /workspace/solastjs
 
-podman pod create --name sol-pod --volume ../:/workspace/solastjs --publish 8080:80
+# run tests using 'npm run test' and coverage 'npm run coverage'
 
-podman pull docker.io/alpine:latest
-podman build --file alpine.containerfile --tag localhost/sol-alpine-img .
-podman run -dt --pod sol-pod --name sol-alpine localhost/sol-alpine-img
+podman pod create --name sol-pod --volume ../:/workspace/solastjs \
+  --publish 8080:80
+
+podman pull docker.io/node:current-alpine
+podman build --file node.containerfile --tag localhost/sol-node-img .
+podman run -dt --pod sol-pod --name sol-node localhost/sol-node-img
 
 podman pull docker.io/nginx:latest
 podman build --file nginx.containerfile --tag localhost/sol-nginx-img .
