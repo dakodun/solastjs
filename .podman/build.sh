4c29a01b19b1c1cb513433217d5a64c02efb3220
Iain Crawford (dakodun) <github@chikin.net>
2024-09-19 14:35:17 +0000

    updated dev container
    nginx now uses alpine 1.27 image
    
    node now uses 22.9 bookworm slim image and installs playwright for browser testing

--- .podman/build.sh
+++ .podman/build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash -x
 
-# simple script for initialising a pod consisting of alpine
-# (w/ npm/vitest & git/openssh) and nginx serving the project directory
-# to localhost:8080
+# simple script for initialising a pod consisting of bookworm-slim
+# (w/ npm/vitest/playwright & git/openssh) and nginx serving the project
+# directory to localhost:8080
 
 # to use, cd into the .podman folder and run the script, attach
 # to the node container (sol-node) with vscode via the dev containers
 # extension and then open folder /workspace/solastjs
 
-# run tests using 'npm run test' and coverage 'npm run coverage'
+# run tests using 'npm run test' or via playwright and headless
+# browser using 'xnpm run test'
 
 podman pod create --name sol-pod --volume ../:/workspace/solastjs \
   --publish 8080:80
 
-podman pull docker.io/node:current-alpine
+podman pull docker.io/node:22.9-bookworm-slim
 podman build --file node.containerfile --tag localhost/sol-node-img .
 podman run -dt --pod sol-pod --name sol-node localhost/sol-node-img
 
-podman pull docker.io/nginx:latest
+podman pull docker.io/nginx:1.27-alpine
 podman build --file nginx.containerfile --tag localhost/sol-nginx-img .
 podman run -dt --pod sol-pod --name sol-nginx localhost/sol-nginx-img
