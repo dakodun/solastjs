a170f2b801d28de49c9269d0ce901b9530c2461d
Iain Crawford (dakodun) <github@chikin.net>
2024-09-25 12:39:48 +0000

    use static fields instead

--- scr/glstates.js
+++ scr/glstates.js
@@ -1,15 +1,11 @@
 import Mat4 from './mat4.js';
 import Shader from './shader.js';
 
-class GLStates_ {
-	constructor() {
-    this.projectionMatrix = new Mat4();
-		this.viewMatrix = new Mat4();
+class GLStates {
+  static projectionMatrix = new Mat4();
+  static viewMatrix = new Mat4();
 
-    this.defaultShader = new Shader();
-	}
+  static defaultShader = new Shader();
 };
-
-var GLStates = new GLStates_();
 
 export default GLStates;
