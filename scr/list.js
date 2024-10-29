117a361b1e8a9d12fe95ebc4c49118795f2b5e43
Iain Crawford (dakodun) <github@chikin.net>
2024-10-29 13:38:07 +0000

    added a little robustness to the list class

--- scr/list.js
+++ scr/list.js
@@ -1,39 +1,83 @@
 class List {
   static Node = class {
-    constructor(data) {
-      this.prev = null;
-      this.next = null;
+    // private fields
+      #prev = null;
+      #next = null;
+    // ...
 
+    constructor(data) {
       this.data = data;
     }
+
+    // getters/setters
+    get prev() { return this.#prev; }
+    get next() { return this.#next; }
+
+    set prev(prev) {
+      if (!(prev instanceof List.Node) && prev !== null) {
+        throw new TypeError("List.Node (prev): should be a " +
+          "List.Node or null");
+      } else if (prev === this) {
+        throw new ReferenceError("List.Node (prev): assigning this " +
+          "to prev creates a cyclic reference");
+      }
+
+      this.#prev = prev;
+    }
+
+    set next(next) {
+      if (!(next instanceof List.Node) && next !== null) {
+        throw new TypeError("List.Node (next): should be a " +
+          "List.Node or null");
+      } else if (next === this) {
+        throw new ReferenceError("List.Node (next): assigning this " +
+          "to next creates a cyclic reference");
+      }
+
+      this.#next = next;
+    }
+    // ...
   };
 
-	constructor() {
-		this.front = null;
-    this.last = null;
-	}
+  // private fields
+    #front = null;
+    #back  = null;
+  // ...
+
+	constructor(nodeData = []) {
+    if (!(nodeData instanceof Array)) {
+      throw new TypeError("List (nodeData): should be an Array");
+    }
+
+    nodeData.forEach((e) => { this.push(e); });
+  }
+
+  // getters/setters
+  get front() { return this.#front; }
+  get back()  { return this.#back;  }
+  // ...
 
   push(data) {
     let node = new List.Node(data);
 
-    if (this.front === null) {
-      this.front = node;
-      this.last = node;
+    if (this.#front === null) {
+      this.#front = node;
+      this.#back = node;
     } else {
-      this.last.next = node;
-      node.prev = this.last;
+      this.#back.next = node;
+      node.prev = this.#back;
 
-      this.last = node;
+      this.#back = node;
     }
   }
 
   delete(node) {
-    if (node === this.front) {
-      this.front = node.next;
+    if (node === this.#front) {
+      this.#front = node.next;
     }
     
-    if (node === this.last) {
-      this.last = node.prev;
+    if (node === this.#back) {
+      this.#back = node.prev;
     }
 
     if (node.next !== null) {
@@ -42,6 +86,14 @@ class List {
 
     if (node.prev !== null) {
       node.prev.next = node.next;
+    }
+  }
+
+  forEach(callbackFn = (n) => {}) {
+    let node = this.#front;
+    while(node !== null) {
+      callbackFn(node);
+      node = node.next;
     }
   }
 };
