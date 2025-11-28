3cf0742d242a1a2dc86d677911a615d05e7f2e19
Iain Crawford (dakodun) <github@chikin.net>
2025-11-28 19:49:58 +0000

    updated camera3d to latest specs
    - fixed an issue with 'lookAt' method and 'position'

--- src/camera3d.js
+++ src/camera3d.js
@@ -1,58 +1,90 @@
+import Sol from './sol.js';
+
 import Mat4 from './mat4.js';
 import Vec3 from './vec3.js';
 
 class Camera3D {
+  // a Camera3D is a 4-dimensional 'view' matrix with
+  // properties and methods that apply translations
+  // and rotations, creating a camera object that can
+  // be moved around a 3-dimensional space
+
+  //> static enums //
   static Plane = {
     YZ : 1,
     XZ : 2,
     XY : 4,
   };
 
-  // private fields
-    #view = new Mat4();
-  // ...
+  //> public properties //
+  update = false;
 
+  //> internal properties //
+  _view = new Mat4();
+
+  _position = new Vec3(0.0, 0.0, 0.0);
+  _rotation = new Vec3(0.0, 0.0, 0.0);
+
+  //> constructor //
 	constructor() {
-    this.update = false;
-
-    this.position = new Vec3(0.0, 0.0, 0.0);
-    this.rotation = new Vec3(0.0, 0.0, 0.0);
+    
 	}
 
-  // getters/setters
+  //> getters/setters //
   get view() {
-    this.#updateView()
-    return this.#view;
+    this._updateView();
+    return this._view;
   }
-  // ...
 
+  get position() { return this._position; }
+  get rotation() { return this._rotation; }
+
+  set position(position) {
+    Sol.CheckTypes(this, "set position",
+    [{position}, [Vec3]]);
+
+    this._position = position;
+    this.update = true;
+  }
+
+  set rotation(rotation) {
+    Sol.CheckTypes(this, "set rotation",
+    [{rotation}, [Vec3]]);
+
+    this._rotation = rotation;
+    this.update = true;
+  }
+
+  //> public methods //
 	copy(other) {
-    this.#view = other.#view.getCopy();
+    this._view = other._view.getCopy();
     this.update = other.update;
 
-    this.position = other.position.getCopy();
-    this.rotation = other.rotation.getCopy();
+    this._position = other._position.getCopy();
+    this._rotation = other._rotation.getCopy();
   }
 
   getCopy() {
-    let copy = new Camera3D(); copy.copy(this);
+    let copy = new Camera3D();
+    copy.copy(this);
+
     return copy;
   }
 
   setPosition(position) {
-    this.position.copy(position);
+    this._position = position;
     
     this.update = true;
   }
 
   setRotation(rotation) {
-    this.rotation = rotation;
+    this._rotation = rotation;
     
     this.update = true;
   }
 
   translate(direction, plane) {
-    this.#updateView();
+    this._updateView();
 
     let axis = new Array(3);
     let dist = direction.asArray();
@@ -60,18 +92,21 @@ class Camera3D {
     for (let i = 0; i < 3; ++i) {
       if (Math.abs(dist[i]) > 1e-15) {
         // get the transformed axis to move along
+
         axis[i] = new Vec3(
           ((plane & Camera3D.Plane.YZ) === 0) ?
-            this.#view.arr[0 + i] : 0,
+            this._view.arr[0 + i] : 0,
           ((plane & Camera3D.Plane.XZ) === 0) ?
-            this.#view.arr[4 + i] : 0,
+            this._view.arr[4 + i] : 0,
           ((plane & Camera3D.Plane.XY) === 0) ?
-            this.#view.arr[8 + i] : 0
-        ); axis[i].normalize();
+            this._view.arr[8 + i] : 0
+        );
+        
+        axis[i].normalize();
 
-        this.position.x += axis[i].x * dist[i];
-        this.position.y += axis[i].y * dist[i];
-        this.position.z += axis[i].z * dist[i];
+        this._position.x += axis[i].x * dist[i];
+        this._position.y += axis[i].y * dist[i];
+        this._position.z += axis[i].z * dist[i];
       }
     }
 
@@ -97,51 +132,52 @@ class Camera3D {
       centerVec.x - eyeVec.x,
       centerVec.y - eyeVec.y,
       centerVec.z - eyeVec.z
-    ); f.normalize();
+    );
+    
+    f.normalize();
 
-    let s = f.getCross(up); s.normalize();
+    let s = f.getCross(up);
+    s.normalize();
     let u = s.getCross(f);
 
     // create the view matrix
-    this.#view.identity();
+    this._view.identity();
 
-    this.#view.arr[ 0] = s.x;
-    this.#view.arr[ 4] = s.y;
-    this.#view.arr[ 8] = s.z;
+    this._view.arr[ 0] = s.x;
+    this._view.arr[ 4] = s.y;
+    this._view.arr[ 8] = s.z;
     
-    this.#view.arr[ 1] = u.x;
-    this.#view.arr[ 5] = u.y;
-    this.#view.arr[ 9] = u.z;
+    this._view.arr[ 1] = u.x;
+    this._view.arr[ 5] = u.y;
+    this._view.arr[ 9] = u.z;
     
-    this.#view.arr[ 2] = -f.x;
-    this.#view.arr[ 6] = -f.y;
-    this.#view.arr[10] = -f.z;
+    this._view.arr[ 2] = -f.x;
+    this._view.arr[ 6] = -f.y;
+    this._view.arr[10] = -f.z;
 
-    this.#view.arr[12] = 0;
-    this.#view.arr[13] = 0;
-    this.#view.arr[14] = 0;
+    this._view.arr[12] = 0;
+    this._view.arr[13] = 0;
+    this._view.arr[14] = 0;
 
-    this.#view.translate(eyeVec.getNegated());
-    // ...
+    this._view.translate(eyeVec.getNegated());
 
     // get the position and rotation from the matrix
-    let decom = this.#view.decompose();
+    // (both need to be negated as we're techincally
+    // moving the camera in the opposite direction)
 
-    this.position.copy(decom[0]);
+    let decom = this._view.decompose();
 
-    this.rotation.copy(decom[2]);
-    this.rotation.x = -this.rotation.x;
-    this.rotation.y = -this.rotation.y;
-    this.rotation.z = -this.rotation.z;
-    // ...
+    this._position = decom[0].getNegated();
+    this._rotation = decom[2].getNegated();
   }
 
-  #updateView() {
+  //> internal methods //
+  _updateView() {
     if (this.update) {
-      this.#view.identity();
+      this._view.identity();
 
-      this.#view.rotateEuler(this.rotation.getNegated());
-      this.#view.translate(this.position.getNegated());
+      this._view.rotateEuler(this._rotation.getNegated());
+      this._view.translate(this._position.getNegated());
 
       this.update = false;
     }
